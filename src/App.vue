018029fbef44d3a2ed80f2f98f8f97926da7dfb3
jaycemxy <jayce_ma.xa@foxmail.com>
2018-08-24 17:21:50 +0800

    init and finished Header component

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Header></Header>   <!-- 组件使用第三步：放在合适的加载位置 -->
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'  // 组件使用第一步：引入
+export default {
+  name: 'App',
+  components: {  // 组件使用第二步：注册
+    Header
+  }
+}
+</script>
+
+<style>
+
+</style>
